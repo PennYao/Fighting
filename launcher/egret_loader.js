721a567a02370ff36dcf688171616a04685b7069
pennyao <penn0709@hotmail.com>
2015-06-14 03:18:40 +0800

    first commit
    Signed-off-by: pennyao <penn0709@hotmail.com>

---
+++ launcher/egret_loader.js
@@ -0,0 +1,89 @@
+//////////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2014-2015, Egret Technology Inc.
+//  All rights reserved.
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the Egret nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY EGRET AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+//  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+//  IN NO EVENT SHALL EGRET AND CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;LOSS OF USE, DATA,
+//  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+//  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//////////////////////////////////////////////////////////////////////////////////////
+
+egret_h5.startGame = function () {
+    var context = egret.MainContext.instance;
+    context.touchContext = new egret.HTML5TouchContext();
+    context.deviceContext = new egret.HTML5DeviceContext();
+    context.netContext = new egret.HTML5NetContext();
+
+    // egret.StageDelegate.getInstance().setDesignSize(480, 800);
+    egret.StageDelegate.getInstance().setDesignSize(720, 1280);
+    context.stage = new egret.Stage();
+    var scaleMode = egret.MainContext.deviceType == egret.MainContext.DEVICE_MOBILE ? egret.StageScaleMode.SHOW_ALL : egret.StageScaleMode.NO_SCALE;
+    // scaleMode = egret.StageScaleMode.SHOW_ALL;// 指定整个应用程序在指定区域中可见，且不会发生扭曲，同时保持应用程序的原始高宽比。应用程序的两侧可能会显示边框。
+    // scaleMode = egret.StageScaleMode.NO_BORDER;// 指定整个应用程序填满指定区域，不会发生扭曲，但有可能会进行一些裁切，同时保持应用程序的原始高宽比。
+    // scaleMode = egret.StageScaleMode.NO_SCALE;// 指定应用程序的大小是固定的，因此，即使在更改播放器窗口大小时，它仍然保持不变。如果播放器窗口比内容小，则可能进行一些裁切。这种模式通常用于PC调试。
+    // scaleMode = egret.StageScaleMode.EXACT_FIT;// 指定整个应用程序在指定区域中可见，但不尝试保持原始高宽比。可能会发生扭曲。
+    context.stage.scaleMode = scaleMode;
+
+    //WebGL is a Egret's beta property. It's off by default.
+    //WebGL是egret的Beta特性，默认关闭
+    var rendererType = 0;
+    if (rendererType == 1) {// egret.WebGLUtils.checkCanUseWebGL()) {
+        console.log("Use WebGL mode");
+        context.rendererContext = new egret.WebGLRenderer();
+    }
+    else {
+        context.rendererContext = new egret.HTML5CanvasRenderer();
+    }
+
+    egret.MainContext.instance.rendererContext.texture_scale_factor = 1;
+    context.run();
+
+    var rootClass;
+    if (document_class) {
+        rootClass = egret.getDefinitionByName(document_class);
+    }
+    if (rootClass) {
+        var rootContainer = new rootClass();
+        if (rootContainer instanceof egret.DisplayObjectContainer) {
+            context.stage.addChild(rootContainer);
+        }
+        else {
+            throw new Error("Document Class must be the subclass to egret.DisplayObjectContainer!");
+        }
+    }
+    else {
+        throw new Error("Document Class is not found！");
+    }
+
+    //处理屏幕大小改变
+    //implement for screen size change
+    var resizeTimer = null;
+    var doResize = function () {
+        context.stage.changeSize();
+        resizeTimer = null;
+    };
+    window.onresize = function () {
+        if (resizeTimer == null) {
+            resizeTimer = setTimeout(doResize, 300);
+        }
+    };
+};
